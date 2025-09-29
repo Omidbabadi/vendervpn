5a7d322063b34cee3a39ec194cb1c3f48816e598
omid5233 <omid5233@gmail.com>
2025-09-29 21:27:48 +0000

    connection button

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -15,20 +15,6 @@
   idx.previews = {
       enable = true;
     previews = {
-      web = {
-        command = [
-          "flutter"
-          "run"
-          "--machine"
-          "-d"
-          "web-server"
-          "--web-hostname"
-          "0.0.0.0"
-          "--web-port"
-          "$PORT"
-        ];
-        manager = "flutter";
-      };
       android = {
         command = [
           "flutter"
